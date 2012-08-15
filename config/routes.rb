0d7a4d39fb60da2b55cfc31e29f733953cf745c6
Toan Nguyen <toannm3110@gmail.com>
2012-08-15 10:37:00 +0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   root to: 'static_page#home'
+
+  match '/signup',  to: 'users#new'
   match '/help',    to: 'static_page#help'
   match '/about',   to: 'static_page#about'
   match '/contact', to: 'static_page#contact'
