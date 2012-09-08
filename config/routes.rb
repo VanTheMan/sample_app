04f8fcd7f673022ffc0c15bcca574b3bd719abc0
Toan Nguyen <toannm3110@gmail.com>
2012-09-09 00:32:22 +0700

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,17 @@ SampleApp::Application.routes.draw do
   match '/about',   to: 'static_page#about'
   match '/contact', to: 'static_page#contact'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   resources :sessions, only: [:new, :create, :destroy]
 
   resources :microposts, only: [:create, :destroy]
+
+  resources :relationships, only: [:create, :destroy]
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
